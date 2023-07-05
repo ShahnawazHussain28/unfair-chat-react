dd8b8731932b1a489321660716ad9dedc5dbe377
ShahnawazHussain28 <shahnawazhussain2802@gmail.com>
2023-07-05 19:12:55 +0530

    Final build

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,5 @@
-// export const URL = "/"
-export const URL = "http://192.168.31.252:5000/"
+export const URL = "/"
+// export const URL = "http://192.168.31.252:5000/"
 
 export async function POST(to, body){
     let res = await fetch(URL+to, {
